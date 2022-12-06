2b5b323279c4ab4e601447cf76196b0ca95143c1
Maximilian <maximilian@kreissl.at>
2022-12-06 20:23:35 +0100

    Removed node_modules also added a .gitignore

--- Server.js
+++ Server.js
@@ -12,22 +12,19 @@ app.get('/', async (req, res) => {
 
     console.log(req.ip)
     console.log(decodeURI(req.query.url))
-
     Jimp.read(decodeURI(req.query.url), function (err, image) {
-        console.log(err)
-        let xscalefactor = width/image.getWidth();
-        let yscalefactor = height/image.getHeight();
-	let scaledx
-        let scaledy
-        if (xscalefactor > yscalefactor) {
-            scaledx = image.getWidth()*yscalefactor
-            scaledy = image.getHeight()*yscalefactor
-        } else {
-            scaledx = image.getWidth()*xscalefactor
-            scaledy = image.getHeight()*xscalefactor
-        }
-        image=image.resize(scaledx,scaledy, Jimp.RESIZE_NEAREST_NEIGHBOR);
-        result += image.getHeight() + ", " + image.getWidth() + ", ";
+        image=image.scale(0.1);
+	console.log(width, height)
+	console.log(image.bitmap.width, image.bitmap.height)
+	console.log(image.getWidth(), image.getHeight())
+	image=image.rotate(-90)
+	image=image.flip(true,false)
+    console.log(err)
+    let result = {};
+    result.height=image.getHeight();
+    result.width=image.getWidth();
+    result.pixels={};
+    //result += image.getHeight() + ", " + image.getWidth() + ", ";
 
 
 
@@ -36,11 +33,12 @@ app.get('/', async (req, res) => {
 	  let green = this.bitmap.data[idx + 1];
 	  let blue = this.bitmap.data[idx + 2];
 	  let alpha = this.bitmap.data[idx + 3];
-	  result += + red + ", " + blue + ", " + green + ", " + alpha + ", ";
+      result.pixels[x][y] = {red,green,blue,alpha};
+	//   result += + red + ", " + blue + ", " + green + ", " + alpha + ", ";
 	});
 
-        console.log(result.substring(0, result.length - 2).length)
-        res.send(result.substring(0, result.length - 2));
+        // console.log(result.substring(0, result.length - 2).length)
+        res.send(JSON.stringify(result));
     });
 })
 
