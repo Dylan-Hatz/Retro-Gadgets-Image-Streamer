8097fc9048c6764c22a8352537b41b96d24b169a
Maximilian <maximilian@kreissl.at>
2022-12-06 22:17:39 +0100

    Fixed the tearing

--- RetroGadgetsScript.lua
+++ RetroGadgetsScript.lua
@@ -4,7 +4,7 @@ local screenheight=vid.Height
 local screenwidth=vid.Width
 
 local server="http://localhost:3000"
-local url = "https://sample-videos.com/img/Sample-jpg-image-500kb.jpg"
+local url = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
 local widthmessage = "&width="..screenwidth
 local heightmessage = "&height="..screenheight
 
@@ -29,13 +29,17 @@ function mysplit (inputstr, sep)
 end
 
 function eventChannel1(sender:Wifi, arg:WifiWebResponseEvent)
+	if(arg.ResponseCode~=200)then
+		logError("Return Code not 200")
+		return
+	end
   list = mysplit(arg.Text, ", ")
   height = table.remove(list, 1)
   width = table.remove(list, 1)
   currentPixel = {0, 0, 0, 0}
   index = 1
-	for pixelx=1, width, 1 do
-		for pixely=1, height, 1 do
+	for pixely=1, height, 1 do
+		for pixelx=1, width, 1 do
 currentPixel={list[index],list[index+1],list[index+2],list[index+3]}
 			vid:SetPixel(vec2(pixelx, pixely), ColorRGBA(currentPixel[1], currentPixel[2], currentPixel[3], currentPixel[4]))
 			index+=4
--- Server.js
+++ Server.js
@@ -9,30 +9,35 @@ app.get('/', async (req, res) => {
     let result = '';
     let width = +req.query.width
     let height = +req.query.height
+    let url = decodeURI(req.query.url)
 
     console.log(req.ip)
-    console.log(decodeURI(req.query.url))
-    Jimp.read(decodeURI(req.query.url), function (err, image) {
-        image=image.resize(width, height);
-	console.log(width, height)
-	console.log(image.bitmap.width, image.bitmap.height)
-	console.log(image.getWidth(), image.getHeight())
-	image=image.rotate(-90)
-	image=image.flip(true,false)
-    image.quality(100)
-    // image=image.dither565()
-    // image=image.normalize()
+    console.log(url)
+    Jimp.read(url, function (err, image) {
         console.log(err)
+
+        let xscalefactor = width/image.getWidth();
+        let yscalefactor = height/image.getHeight();
+        let scaledx
+        let scaledy
+        if (xscalefactor > yscalefactor) {
+            scaledx = image.getWidth()*yscalefactor
+            scaledy = image.getHeight()*yscalefactor
+        } else {
+            scaledx = image.getWidth()*xscalefactor
+            scaledy = image.getHeight()*xscalefactor
+        }
+        image=image.resize(scaledx, scaledy)
         result += image.getHeight() + ", " + image.getWidth() + ", ";
 
 
-	image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
-	  let red = this.bitmap.data[idx + 0];
-	  let blue = this.bitmap.data[idx + 1];
-	  let green = this.bitmap.data[idx + 2];
-	  let alpha = this.bitmap.data[idx + 3];
-	  result += + red + ", " + blue + ", " + green + ", " + alpha + ", ";
-	});
+        image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
+            let red = this.bitmap.data[idx + 0];
+            let blue = this.bitmap.data[idx + 1];
+            let green = this.bitmap.data[idx + 2];
+            let alpha = this.bitmap.data[idx + 3];
+            result += + red + ", " + blue + ", " + green + ", " + alpha + ", ";
+        });
 
         console.log(result.substring(0, result.length - 2).length)
         res.send(result.substring(0, result.length - 2));
